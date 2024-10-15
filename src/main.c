52196364c598068f5a926050ab21b99a37e8bd70
coenicorn <coenicorn>
2024-10-15 15:52:13 +0200

    bababoei

--- src/main.c
+++ src/main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <raylib.h>
 
 int main(void) {
-    printf("Hello World!");
+
+    InitWindow(500, 500, "surgen");
+    SetTargetFPS(1000);
+
+    while (!WindowShouldClose())
+    {
+
+        BeginDrawing();
+
+            DrawRectangle(0, 0, 100, 100, RED);
+
+        EndDrawing();
+
+    }
+
+    CloseWindow();
 
     return 0;
 }
